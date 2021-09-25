ad6ec77a6762bd005b950d6c4f5d7f23e8f2ad89
Aravind K B <73105116+kannanhacker123@users.noreply.github.com>
2021-09-25 21:20:48 +0530

    Update passwd_genaretor.py

--- passwd_genaretor.py
+++ passwd_genaretor.py
@@ -6,7 +6,11 @@ signs = "~`|•√π÷×¶∆£¢€¥^°={}\%©®™✓[]@#$_&-+()/*':;!?"
 all = lower+upper+number+signs
 print("Enter the length of your password.")
 print("Short password is better to break.")
+print("So make strong password")
+
 print("pls be sure your password is between 8-50")
+print("how many characters do you want ?)
+
 length = int(input("Aravind@061==> ")
 
 print("".join(random.sample(all,length)
